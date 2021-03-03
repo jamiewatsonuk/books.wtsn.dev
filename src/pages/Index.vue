24f3a429d61336c7d1f85f89acc2a868aa167f85
Jamie Watson <jamiewatson07@gmail.com>
2021-03-03 21:11:22 +0000

    Layouts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="bg-gray-100 min-h-screen">
+  <div class="bg-gray-100 min-h-screen antialias font-sans text-gray-900">
     <div class="bg-gray-900 text-white text-center p-2 text-xs">
-      A little experiment using Gridsome and Puppeteer 💪
+      
     </div>
-    <div class=" grid grid-cols-3 md:grid-cols-4 lg:grid-cols-9 auto-rows-min">
+
+    <div class="p-6 pb-0">
+      <div class="font-serif text-2xl">{{ $page.books.edges.length }} books so far</div>
+      <div class="text-gray-500">A little experiment using Gridsome and Puppeteer 💪</div>
+    </div>
+    <div class=" grid grid-cols-3 md:grid-cols-4 lg:grid-cols-8 gap-6 auto-rows-min p-6">
       <div v-for="edge in $page.books.edges" :key="edge.node.title">
-        <div class="w-full relative" style="padding-bottom: 150%">
+        <div class="w-full relative overflow-hidden rounded shadow-2xl" style="padding-bottom: 150%">
           <g-image
             :src="edge.node.localImage"
             class="absolute w-full h-full object-cover"
